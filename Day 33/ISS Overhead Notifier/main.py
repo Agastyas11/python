cba0ad5d2db42d1d896a0d46f20de284fcdb410d
Agastya Dubey <agastyas11@gmail.com>
2024-11-23 14:50:11 +1100

    completed day 33 and moved flies

---
+++ Day 33/ISS Overhead Notifier/main.py
@@ -0,0 +1,45 @@
+import requests, time
+from datetime import datetime
+
+MY_LAT = -33.711860
+MY_LONG = 151.054580
+
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    # Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    reponse = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    reponse.raise_for_status()
+    data = reponse.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+while True:
+    if is_night() and is_iss_overhead():
+        print("Look Up")
+        time.sleep(60)
+
+
